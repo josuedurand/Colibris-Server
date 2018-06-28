2bcd86a16e424cdd0012c4b6d2231e9fa445bf17
unknown <jonathan.jourdan@hotmail.fr>
2018-06-28 10:31:46 +0200

    Add controler model

---
+++ app/api/models/models.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+
+const allModels = {};
+
+// chargement des schémas depuis le fichier de configuration JSON dans une variable
+const database_models = JSON.parse(fs.readFileSync("app/api/models/models.json", 'utf8'));
+
+// Initialisation de chaque schéma par association entre le schéma et la collection
+for (modelName in database_models) {
+    allModels[modelName] = mongoose.model(modelName, database_models[modelName].schema,
+        database_models[modelName].collection);
+    console.log("model "+ modelName + " chargé !");
+}
+
+console.log(allModels["Users"].schema);
+
+module.exports = allModels;
+
+
+
