124b44a3a9f060c9bb42ba84742b1da8fa04cb79
josue.durand <josue.durand@GSA-PF0KMS6D.GSA>
2018-07-03 09:37:48 +0100

    correction tableau d'oid users

--- controllers/users.js
+++ controllers/users.js
@@ -6,44 +6,58 @@ const
 	ObjectId = require('mongoose').Types.ObjectId;
 
 module.exports = {
-getAll: function (req, res) {
-	models["Users"].find({})
-		.populate('colleges_extId')
-		.lean()
-		.exec(function (error, result) {
-			if (error) {
-				next(error);
-			} else {
-				res.status(200).json({
-					status: "success",
-					message: "Liste des utilisateurs trouvée avec succés.",
-					data: { users : result }
-				});
-			}
-		});
-},
-getById: function (req, res, next) {
-	// console.log(req.body);
-	models['Users'].findById(req.params.userId)
-		.populate('colleges_extId')
-		.lean()
-		.exec(function (error, result) {
-			if (error) {
-				next(error);
-			} else {
-				if (!result) {
-					res.json({ status: "success", message: `Utilisateur ${req.params.userId} introuvable.`, data: { user: result } });
+	getAll: function (req, res) {
+		models["Users"].find({})
+			.populate('colleges_extId')
+			.lean()
+			.exec(function (error, result) {
+				if (error) {
+					next(error);
 				} else {
-					res.json({ status: "success", message: `Utilisateur ${req.params.userId} trouvé avec succés.`, data: { user: result } });
+					res.status(200).json({
+						status: "success",
+						message: "Liste des utilisateurs trouvée avec succés.",
+						data: {	users: result }
+					});
 				}
-			}
-		});
-},
-create: function (req, res, next) {
-	
-	console.log('req.body', req.body);
+			});
+	},
+	getById: function (req, res, next) {
+		// console.log(req.body);
+		models['Users'].findById(req.params.userId)
+			.populate('colleges_extId')
+			.lean()
+			.exec(function (error, result) {
+				if (error) {
+					next(error);
+				} else {
+					if (!result) {
+						res.json({
+							status: "success",
+							message: `Utilisateur ${req.params.userId} introuvable.`,
+							data: { user: result }
+						});
+					} else {
+						res.json({
+							status: "success",
+							message: `Utilisateur ${req.params.userId} trouvé avec succés.`,
+							data: { user: result }
+						});
+					}
+				}
+			});
+	},
+	create: function (req, res, next) {
 
-	models["Users"].create({
+		console.log('req.body', req.body);
+		let collegesArray = [];
+		for (let index = 0; index < req.body.colleges.length; index++) {
+			collegesArray.push({ _id: new ObjectId( req.body.colleges[index] ) })
+			
+		}
+		// [{ _id: new ObjectId(req.body.colleges) }]
+
+		models["Users"].create({
 			_id: new ObjectId(),
 			civility: req.body.civility,
 			lastName: req.body.lastName,
@@ -51,53 +65,82 @@ create: function (req, res, next) {
 			email: req.body.email,
 			password: req.body.password,
 			profil: req.body.profil,
-			colleges_extId: { _id: new ObjectId(req.body.colleges) }
+			colleges_extId: collegesArray
 		}, function (err, result) {
-		if (err) {
-			next(err);
-		} else {
-			res.json({ status: "success", message: "Utilisateur ajouté avec succés", data: null });
-		}
-	});
-},
-updateById: function (req, res, next) {
-	models['Users'].findByIdAndUpdate(req.params.userId, { 
-		civility: req.body.civility,
-		lastName: req.body.lastName,
-		firstName: req.body.firstName,
-		email: req.body.email,
-		password: req.body.password,
-		profil: req.body.profil,
-		colleges_extId: req.body.colleges
-	}, function (err, user) {
-		if (err)
-			next(err);
-		else {
-			res.json({ status: "success", message: `Utilisateur ${req.params.userId} mis à jour avec succés.`, data: null });
-		}
-	});
-},
-deleteById: function (req, res, next) {
-	models['Users'].findByIdAndRemove(req.params.userId, function (err, user) {
-		if (err)
-			next(err);
-		else {
-			res.json({ status: "success", message: `Utilisateur ${req.params.userId} supprimé avec succés.`, data: null });
-		}
-	});
-},
-authenticate: function (req, res, next) {
-	models["Users"].findOne({ email: req.body.email }, function (err, userInfo) {
-		if (err) {
-			next(err);
-		} else {
-			if (bcrypt.compareSync(req.body.password, userInfo.password)) {
-				const token = jwt.sign({ id: userInfo.userId }, req.app.get('secretKey'), { expiresIn: '1h' });
-				res.json({ status: "success", message: "Utilisateur trouvé avec succés.", data: { user: userInfo, token: token } });
+			if (err) {
+				next(err);
 			} else {
-				res.json({ status: "error", message: "email/password invalide.", data: null });
+				res.json({
+					status: "success",
+					message: "Utilisateur ajouté avec succés",
+					data: null
+				});
 			}
-		}
-	});
-}
+		});
+	},
+	updateById: function (req, res, next) {
+		models['Users'].findByIdAndUpdate(req.params.userId, {
+			civility: req.body.civility,
+			lastName: req.body.lastName,
+			firstName: req.body.firstName,
+			email: req.body.email,
+			password: req.body.password,
+			profil: req.body.profil,
+			colleges_extId: req.body.colleges
+		}, function (err, user) {
+			if (err)
+				next(err);
+			else {
+				res.json({
+					status: "success",
+					message: `Utilisateur ${req.params.userId} mis à jour avec succés.`,
+					data: null
+				});
+			}
+		});
+	},
+	deleteById: function (req, res, next) {
+		models['Users'].findByIdAndRemove(req.params.userId, function (err, user) {
+			if (err)
+				next(err);
+			else {
+				res.json({
+					status: "success",
+					message: `Utilisateur ${req.params.userId} supprimé avec succés.`,
+					data: null
+				});
+			}
+		});
+	},
+	authenticate: function (req, res, next) {
+		models["Users"].findOne({
+			email: req.body.email
+		}, function (err, userInfo) {
+			if (err) {
+				next(err);
+			} else {
+				if (bcrypt.compareSync(req.body.password, userInfo.password)) {
+					const token = jwt.sign({
+						id: userInfo.userId
+					}, req.app.get('secretKey'), {
+						expiresIn: '1h'
+					});
+					res.json({
+						status: "success",
+						message: "Utilisateur trouvé avec succés.",
+						data: {
+							user: userInfo,
+							token: token
+						}
+					});
+				} else {
+					res.json({
+						status: "error",
+						message: "email/password invalide.",
+						data: null
+					});
+				}
+			}
+		});
+	}
 }
--- models/models.js
+++ models/models.js
@@ -9,8 +9,11 @@ const database_models = JSON.parse(fs.readFileSync('models/models.json', 'utf8')
 
 // Initialisation de chaque schéma par association entre le schéma et la collection
 for (modelName in database_models) {
-    allModels[modelName] = mongoose.model(modelName, database_models[modelName].schema,
-        database_models[modelName].collection);
+    allModels[modelName] = mongoose.model(
+            modelName,
+            database_models[modelName].schema,
+            database_models[modelName].collection
+        );
     console.log("model "+ modelName + " chargé !");
 }
 
